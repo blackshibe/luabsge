f15bddf3669b08251b408c8f8ad283ccca95701b
Kacper Szczepan <kacperszczepan@tutanota.com>
2026-03-19 15:21:39 +0100

    windows build support

---
+++ setup_win.sh
@@ -0,0 +1,3 @@
+cmake -B build -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe"                           
+cmake -B build \
+    -DCMAKE_BUILD_TYPE=Debug
